4c3dd6187acd7db0924804689398c25ead55460e
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-10-20 13:38:08 -0700

    bat

--- pi/config.fish
+++ pi/config.fish
@@ -1,3 +1,4 @@
 source /usr/local/share/autojump/autojump.fish
 
 abbr l exa --long --header --git
+abbr c bat
--- pi/install.sh
+++ pi/install.sh
@@ -2,13 +2,11 @@
 
 set -euxo pipefail
 
-echo "upgrade-system "
 sudo bash -c "yes | apt-get update"
 sudo bash -c "yes | apt-get full-upgrade"
 
 PACKAGES=(
   autojump
-  bat
   docker
   exa
   fail2ban
@@ -26,6 +24,20 @@ PACKAGES=(
 for package in "${PACKAGES[@]}"; do
   sudo bash -c "yes | apt-get install ${package}"
 done
+
+# install bat
+TMP_DIR=$(mktemp -d)
+cleanup() {
+  rm -rf "$TMP_DIR"
+}
+trap cleanup EXIT
+
+DEB_FILE="${TMP_DIR}/bat.deb"
+BAT_VERSION="${BAT_VERSION:-"0.16.0"}"
+
+wget -O "$DEB_FILE" "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat_${BAT_VERSION}_armhf.deb"
+sudo bash -c "yes | apt-get install libgcc1-armhf-cross"
+sudo bash -c "yes | apt install ${DEB_FILE}"
 
 # install log2ram
 echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
