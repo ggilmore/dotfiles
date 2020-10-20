cc4f04015292befa1251529fed719a7add45efca
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-10-20 13:47:22 -0700

    fd

--- pi/config.fish
+++ pi/config.fish
@@ -2,3 +2,5 @@ source /usr/local/share/autojump/autojump.fish
 
 abbr l exa --long --header --git
 abbr c bat
+
+set -x BAT_THEME 'GitHub'
--- pi/install.sh
+++ pi/install.sh
@@ -2,15 +2,21 @@
 
 set -euxo pipefail
 
+TMP_DIR=$(mktemp -d)
+cleanup() {
+  rm -rf "$TMP_DIR"
+}
+trap cleanup EXIT
+
 sudo bash -c "yes | apt-get update"
 sudo bash -c "yes | apt-get full-upgrade"
 
 PACKAGES=(
   autojump
   docker
+  emacs-nox
   exa
   fail2ban
-  fd
   fish
   fzf
   git
@@ -26,18 +32,19 @@ for package in "${PACKAGES[@]}"; do
 done
 
 # install bat
-TMP_DIR=$(mktemp -d)
-cleanup() {
-  rm -rf "$TMP_DIR"
-}
-trap cleanup EXIT
-
-DEB_FILE="${TMP_DIR}/bat.deb"
+BAT_DEB_FILE="${TMP_DIR}/bat.deb"
 BAT_VERSION="${BAT_VERSION:-"0.16.0"}"
 
-wget -O "$DEB_FILE" "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat_${BAT_VERSION}_armhf.deb"
+wget -O "${BAT_DEB_FILE}" "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat_${BAT_VERSION}_armhf.deb"
 sudo bash -c "yes | apt-get install libgcc1-armhf-cross"
-sudo bash -c "yes | apt install ${DEB_FILE}"
+sudo bash -c "yes | apt install ${BAT_DEB_FILE}"
+
+# install fd
+FD_DEB_FILE="${TMP_DIR}/fd.deb"
+FD_VERSION="${FD_VERSION:-"8.1.1"}"
+
+wget -O "${FD_DEB_FILE}" "https://github.com/sharkdp/fd/releases/download/v${FD_VERSION}/fd_  ${FD_VERSION}_armhf.deb"
+sudo bash -c "yes | apt install ${FD_DEB_FILE}"
 
 # install log2ram
 echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
