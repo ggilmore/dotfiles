853ba0c4eeb153d5a1f062f7b4db0291be7d7b67
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-10-20 13:55:39 -0700

    fix autojump path

--- pi/setup-fish.sh
+++ pi/setup-fish.sh
@@ -7,7 +7,7 @@ FISH_CONFIG_FILE="${HOME}/.config/fish/config.fish"
 
 mkdir -p "$(dirname "${FISH_CONFIG_FILE}")"
 
-FISH_AUTO_JUMP_FILE="/usr/local/share/autojump/autojump.fish"
+FISH_AUTO_JUMP_FILE="/usr/share/autojump/autojump.fish"
 
 if [ ! -f "$FISH_AUTO_JUMP_FILE" ]; then
   echo "$FISH_AUTO_JUMP_FILE does not exist."
