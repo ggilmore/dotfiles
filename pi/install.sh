e04804ffcb905220cec4f9447fd71c2622fd09b2
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-10-20 13:13:59 -0700

    pi: bash -cf for "yes"

--- pi/install.sh
+++ pi/install.sh
@@ -3,8 +3,8 @@
 set -euxo pipefail
 
 echo "upgrade-system "
-yes | sudo apt-get update
-yes | sudo apt-get full-upgrade
+sudo bash -c "yes | apt-get update"
+sudo bash -c "yes | apt-get full-upgrade"
 
 PACKAGES=(
   autojump
@@ -18,19 +18,20 @@ PACKAGES=(
   git
   lftp
   parallel
+  tmux
   ufw
   vim
 )
 
 for package in "${PACKAGES[@]}"; do
-  yes | sudo apt-get install "${package}"
+  sudo bash -c "yes | apt-get install ${package}"
 done
 
 # install log2ram
 echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
 wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add --yes -
-yes | sudo apt update
-yes | sudo apt install log2ram
+sudo bash -c "yes | apt update"
+sudo bash -c "yes | apt install log2ram"
 
 DEV_FOLDER="${HOME}/dev/github.com/ggilmore"
 
